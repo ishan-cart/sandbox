bd32fca892fdea860cb3299f2eb6d340d3515fb5
ishan <isawant@cartology.com.au>
2024-01-08 15:36:02 +1100

    update bucket

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -4,4 +4,22 @@ resource "google_storage_bucket" "static-site" {
   force_destroy = true
 
   uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
