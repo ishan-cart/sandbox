3e7acf547a2443e385cc70c61b9c81047537c457
ishan <ishan.sawant95@gmail.com>
2026-04-12 19:35:54 +1000

    add github oidc provider and role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,9 @@ variable "environment" {
   type    = string
   default = "development"
 }
+
+variable "github_oidc_subject_pattern" {
+  type        = string
+  description = "StringLike match for token.actions.githubusercontent.com:sub (tighten to your org/repo)."
+  default     = "repo:*/*:*"
+}
