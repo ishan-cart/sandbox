7a0f27ae8257c1150a48037406b6ce26a64a9e08
ishan <ishan.sawant95@gmail.com>
2026-05-07 16:43:16 +1000

    add helm provider

--- terraform/kubernetes/providers.tf
+++ terraform/kubernetes/providers.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.1.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1.1"
+    }
+  }
+  backend "s3" {}
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
@@ -5,5 +20,17 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1"
     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--profile", "k8s-admin"]
     command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--profile", "k8s-admin"]
+      command     = "aws"
+    }
   }
 }
