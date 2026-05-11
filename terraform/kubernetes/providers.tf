cd0748bb600dcfcc2062e3a73f9e84b720ea7e92
ishan <ishan.sawant95@gmail.com>
2026-05-11 15:40:35 +1000

    fix terraform eks auth for gh actions

--- terraform/kubernetes/providers.tf
+++ terraform/kubernetes/providers.tf
@@ -13,12 +13,21 @@ terraform {
   backend "s3" {}
 }
 
+data "terraform_remote_state" "infra" {
+  backend = "s3" # or "remote" for HCP Terraform
+  config = {
+    bucket = "yeahboi-dev-state"
+    key    = "infra/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   exec {
     api_version = "client.authentication.k8s.io/v1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--profile", "k8s-admin"]
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--role-arn", data.terraform_remote_state.infra.outputs.eks_role_arn]
     command     = "aws"
   }
 }
@@ -29,7 +38,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     exec = {
       api_version = "client.authentication.k8s.io/v1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--profile", "k8s-admin"]
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--role-arn", data.terraform_remote_state.infra.outputs.eks_role_arn]
       command     = "aws"
     }
   }
