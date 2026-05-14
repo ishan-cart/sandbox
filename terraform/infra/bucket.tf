4b2d9fdc53fe47ef9905a6f1aa47d2c5b42dbe2f
ishan <ishan.sawant95@gmail.com>
2026-05-14 18:37:09 +1000

    relocate lb access logs bucket

--- terraform/infra/bucket.tf
+++ terraform/infra/bucket.tf
@@ -26,3 +26,43 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "access_logs" {
+  bucket = "${local.env_vars[var.environment].project}-${local.env_vars[var.environment].env_short}-access-logs"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "access_logs" {
+  bucket = aws_s3_bucket.access_logs.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "access_logs" {
+  bucket = aws_s3_bucket.access_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "allow_lb_access" {
+  bucket = aws_s3_bucket.access_logs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AWSLogDeliveryWrite",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "logdelivery.elasticloadbalancing.amazonaws.com"
+        },
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${aws_s3_bucket.access_logs.bucket}/AWSLogs/${local.env_vars[var.environment].project_id}/*",
+      }
+    ]
+  })
+}
