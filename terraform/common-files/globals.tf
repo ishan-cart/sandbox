3629ea87f361573d72f7ca5be0c76825e9e293b2
ishan <ishan.sawant95@gmail.com>
2026-05-20 23:10:22 +1000

    move domain vars to global

--- terraform/common-files/globals.tf
+++ terraform/common-files/globals.tf
@@ -5,6 +5,8 @@ locals {
       project          = "yeahboi"
       project_id       = 537239034666
       eks_cluster_name = "yeahboi-dev-cluster"
+      domain_name      = "ishans.au"
+      zone_id          = "8faa4ea9e5d22c52c4398a3145daef4e"
     }
   }
 }
