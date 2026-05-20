0496a3380723cd4e06bbba45351721bafc87eae2
ishan <ishan.sawant95@gmail.com>
2026-05-20 12:25:00 +1000

    add cloudflare terraform

---
+++ terraform/infra/cloudflare.tf
@@ -0,0 +1,58 @@
+data "cloudflare_ip_ranges" "ip_ranges" {
+  networks = "networks"
+}
+
+resource "cloudflare_dns_record" "cname_root" {
+  zone_id = local.env_vars[var.environment].zone_id
+  name = "@"
+  ttl = 1
+  type = "CNAME"
+  content = aws_lb.front_end.dns_name
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "cname_www" {
+  zone_id = local.env_vars[var.environment].zone_id
+  name = "www"
+  ttl = 1
+  type = "CNAME"
+  content = aws_lb.front_end.dns_name
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "cname_prometheus" {
+  zone_id = local.env_vars[var.environment].zone_id
+  name = "prometheus"
+  ttl = 1
+  type = "CNAME"
+  content = aws_lb.front_end.dns_name
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "cname_grafana" {
+  zone_id = local.env_vars[var.environment].zone_id
+  name = "grafana"
+  ttl = 1
+  type = "CNAME"
+  content = aws_lb.front_end.dns_name
+  proxied = true
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_cloudflare_ips" {
+  for_each = toset(data.cloudflare_ip_ranges.ip_ranges.ipv4_cidrs)
+  security_group_id = aws_security_group.fe_lb.id
+  description       = "Allow https inbound from cloudflare"
+  cidr_ipv4         = each.value
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_https" {
+  security_group_id = aws_security_group.fe_lb.id
+  description       = "Allow https inbound from my ip"
+  cidr_ipv4         = "159.196.168.43/32"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
