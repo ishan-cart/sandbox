3554be2e6c8254d1df25e394a75e479012855392
ishan <ishan.sawant95@gmail.com>
2026-05-20 22:39:32 +1000

    fix output

--- .github/scripts/output-changes.sh
+++ .github/scripts/output-changes.sh
@@ -5,13 +5,12 @@ set -euo pipefail
 terraform=()
 
 for arg in "$@"; do
-
-  if [ "${arg##-*}" == "terraform" ]; then
-    terraform+=("${f##*-}")
+  if [[ "$arg" == terraform* ]]; then
+    terraform+=("${arg##*-}")
   fi
 done
 
-json_output=$(jq -c --args "${terraform[@]}")
+json_output=$(jq -n '$ARGS.positional' -c --args "${terraform[@]}")
 
 echo "changes<<EOF"
 echo "$json_output"
