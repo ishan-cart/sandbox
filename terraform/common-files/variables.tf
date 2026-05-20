4f3feb556a445ed2bc5901c60fdf27af86bbe1ad
ishan <ishan.sawant95@gmail.com>
2026-05-20 17:45:17 +1000

    remove redundant var

--- terraform/common-files/variables.tf
+++ terraform/common-files/variables.tf
@@ -7,9 +7,3 @@ variable "environment" {
   type    = string
   default = "development"
 }
-
-variable "github_oidc_subject_pattern" {
-  type        = string
-  description = "StringLike match for token.actions.githubusercontent.com:sub (tighten to your org/repo)."
-  default     = "repo:*/*:*"
-}
