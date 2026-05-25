206879e01a1c338dbf6d2723c44e01f783591d47
ishan <ishan.sawant95@gmail.com>
2026-05-26 02:52:41 +1000

    lint

--- terraform/kubernetes/kubernetes.tf
+++ terraform/kubernetes/kubernetes.tf
@@ -250,11 +250,11 @@ resource "kubernetes_manifest" "eso_helm_ecr_secret" {
 }
 
 resource "helm_release" "aws_ebs_csi_driver" {
-  name             = "aws-ebs-csi-driver"
-  repository       = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-  chart            = "aws-ebs-csi-driver"
-  version          = "2.60.0"
-  namespace        = "kube-system"
+  name       = "aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  version    = "2.60.0"
+  namespace  = "kube-system"
 
   set = [
     {
