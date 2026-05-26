ddc35b1a662d8ada7b1d81190155fa4bf1fbaf2f
ishan <ishan.sawant95@gmail.com>
2026-05-26 17:07:10 +1000

    udpate script for debugging

--- .docker-compose/snapshot-cron/script.sh
+++ .docker-compose/snapshot-cron/script.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 PROMETHEUS_HOST=${PROMETHEUS_HOST:-localhost}
 PROMETHEUS_PORT=${PROMETHEUS_PORT:-9090}
-PROMETHEUS_HOST=${PROMETHEUS_HOST:-localhost}
 
-BACKUP_QUERY="sum(max by (cluster, namespace,od, container)(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_rate5m)) by (namespace)"
+# FIX: Wrapped the query string in single quotes so inner double quotes don't break it
+BACKUP_QUERY='sum(max by (cluster, namespace, pod, container) (container_memory_rss{job="kubelet", metrics_path="/metrics/cadvisor", container!=""})) by (namespace)'
 QUERY=${QUERY:-$BACKUP_QUERY}
 
-
-curl -fs -X POST http://${PROMETHEUS_HOST}:${PROMETHEUS_PORT}/api/v1/query_range \
+# Executing the POST request to query_range
+curl -f -X POST "http://${PROMETHEUS_HOST}:${PROMETHEUS_PORT}/api/v1/query_range" \
     --data-urlencode "query=${QUERY}" \
     --data-urlencode "start=$(($(date +%s) - 600))" \
     --data-urlencode "end=$(date +%s)" \
